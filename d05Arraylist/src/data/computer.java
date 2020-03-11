795e1dc1c1bdcdfe4bfa51a3cce36a75ddb2c828
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2020-03-11 17:27:14 +0700

    Create computer.java

---
+++ d05Arraylist/src/data/computer.java
@@ -0,0 +1,48 @@
+package data;
+
+import java.util.Scanner;
+
+
+public class computer {
+    //Khai báo thuộc tính của 1 computer
+    public String id,description;
+    public int price;
+    
+    //Hàm dựng không tham số - Default constructor
+    public computer() {
+        id="C01";
+        description="Desktop IBM - 1234";
+        price=100;
+    }
+    
+    //Hàm dựng có tham số
+
+    public computer(String id, String description, int price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+    
+    //Hàm nhập dữ liệu cho 1 máy tính bất kỳ
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("NHập id : ");
+        id=sc.nextLine();
+        
+        System.out.print("Nhập đơn giá : ");
+        price = sc.nextInt();
+        sc=new Scanner(System.in);
+        
+        System.out.print("Nhập mô tả : ");
+        description = sc.nextLine() ;
+        
+        
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %d", id,description,price);
+    }
+    
+    
+}
